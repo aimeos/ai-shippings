35d8a987dc403f17cf9a467d8d3e6c3ed273a2d0
Aimeos <aimeos@aimeos.org>
2022-10-15 17:08:54 +0200

    Initial import

---
+++ manifest.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+	'name' => 'ai-logsta',
+	'config' => [
+		'config',
+	],
+	'depends' => [
+		'aimeos-core',
+	],
+	'include' => [
+		'src',
+	],
+	'i18n' => [
+		'mshop' => 'i18n',
+	],
+	'setup' => [
+	],
+	'template' => [
+	],
+	'custom' => [
+	],
+];
